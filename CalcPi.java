41862ca2baf2bee24ebcc8794629acd645f7cd72
Tal Vered <tal.vered@post.runi.ac.il>
2024-11-19 20:42:02 +0200

    bug fixes calcpi

--- CalcPi.java
+++ CalcPi.java
@@ -9,6 +9,6 @@ public class CalcPi {
 			sign *= -1;
 		}
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated:      " + (4 * qurterPi));		
+		System.out.println("pi, approximated:     " + (4 * qurterPi));
 	}
 }
